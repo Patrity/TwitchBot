11e2a34e101832b1c1fc12d08c8b81629465df68
Tony <tonycos92@gmail.com>
2021-01-13 18:21:16 -0600

    Helper Objects

---
+++ src/main/java/model/Guild.java
@@ -0,0 +1,13 @@
+package model;
+
+/*
+ * @project TwitchBot
+ * @author Patrity - https://github.com/Patrity
+ * Created on - 1/11/2021
+ */
+public class Guild {
+
+    public Guild (String guildId, String channelId){
+
+    }
+}
---
+++ src/main/java/model/Streamer.java
@@ -0,0 +1,19 @@
+package model;
+
+import lombok.Getter;
+
+/*
+ * @project TwitchBot
+ * @author Patrity - https://github.com/Patrity
+ * Created on - 1/11/2021
+ */
+public class Streamer {
+
+    public Streamer(String discordId, String twitchId, String guildId) {
+
+    }
+
+    @Getter
+    public String discordId, twitchId, guildId;
+
+}
